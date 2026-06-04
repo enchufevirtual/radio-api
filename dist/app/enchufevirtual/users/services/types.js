4a21283163cf2ebcdf057bc2f97b34c2daa2aad2
soychendo <chendoec@live.com>
2026-06-04 15:33:44 -0500

    stop tracking dist and force fresh build

--- dist/app/enchufevirtual/users/services/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Status = void 0;
-var Status;
-(function (Status) {
-    Status[Status["PENDING"] = 0] = "PENDING";
-    Status[Status["CONFIRMED"] = 1] = "CONFIRMED";
-})(Status = exports.Status || (exports.Status = {}));
-//# sourceMappingURL=types.js.map
--- dist/enchufevirtual/users/services/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Status = void 0;
-var Status;
-(function (Status) {
-    Status[Status["PENDING"] = 0] = "PENDING";
-    Status[Status["CONFIRMED1"] = 1] = "CONFIRMED1";
-})(Status = exports.Status || (exports.Status = {}));
-//# sourceMappingURL=types.js.map
