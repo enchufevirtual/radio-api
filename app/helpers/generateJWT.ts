4a60032f57c010019ba2b8ff315cc42162d7e5dd
soychendo <chendoec@live.com>
2026-06-06 10:56:10 -0500

    Fixed id login

--- app/helpers/generateJWT.ts
+++ app/helpers/generateJWT.ts
@@ -1,11 +1,10 @@
 import jwt from 'jsonwebtoken';
 import { config } from '../config';
 
-const generateJWT = (id: string) => {
-  
-  return jwt.sign({id}, config.jwtSecret, {
+const generateJWT = (id: string | number) => {
+  return jwt.sign({ id: String(id) }, config.jwtSecret, {
     expiresIn: '7d'
-  })
+  });
 }
 
 export { generateJWT };
